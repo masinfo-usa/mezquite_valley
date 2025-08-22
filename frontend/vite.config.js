dfe8469e55ae9996009ec1bc4b632fcb683b0eed
masinfo-usa <masinfo.usa@gmail.com>
2025-08-21 20:09:35 -0400

    mctl

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/mezquite_valley/',
   server: {
     host: true,
     proxy:{
